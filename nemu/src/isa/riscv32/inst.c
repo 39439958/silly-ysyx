a95e2a3745586d84f0e4a47a2e199d3d38da0ed1
tracer-ysyx <tracer@ysyx.org>
2023-10-29 11:41:48 +0800

    > compile NEMU ysyx_22040000 xuli Linux silly-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:41:48 up  3:13,  1 user,  load average: 0.11, 0.04, 0.01

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -56,7 +56,6 @@ static int decode_exec(Decode *s) {
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
-  Log("s->pc = %x,s->dpc:%x", s->pc,s->dnpc); \
   __VA_ARGS__ ; \
 }
 
