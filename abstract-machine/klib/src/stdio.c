68b4fa787dbe4ef8465a04c3f114186744b03a35
tracer-ysyx <tracer@ysyx.org>
2023-11-04 15:02:57 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:57 up  6:52,  1 user,  load average: 0.07, 0.05, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,47 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+
+  while (*fmt != '\0') {
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 's') {
+        char *tmp_s = va_arg(ap, char*);
+        while (*tmp_s != '\0') {
+          *out++ = *tmp_s++;
+        }
+      }
+      else if (*fmt == 'd') {
+        int tmp_int = va_arg(ap, int);
+        int number = tmp_int;
+
+        int len  = 0;
+        do {
+          number /= 10;
+          len++;
+        } while (number);
+
+        out = out + len - 1;
+        int tmp_len = len;
+        while (tmp_len--) {
+          int tmp = tmp_int % 10;
+          *out-- = tmp + 48;
+          tmp_int /= 10;
+        }
+        out += len;
+      }
+      else {
+        return -1;
+      }
+    }
+    else {
+      *out++ = *fmt;
+    }
+    fmt++;
+  }
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
