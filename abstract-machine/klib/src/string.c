fac8bb5ce6009be82f97e7be630e3ba06bd16da9
tracer-ysyx <tracer@ysyx.org>
2023-11-04 13:04:15 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:04:14 up  4:53,  1 user,  load average: 0.09, 0.04, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -78,35 +78,18 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  assert(n > 0);
-  if (n == 0) 
+  assert(s1 != NULL && s2 != NULL && n >= 0);
+  if (n == 0)
     return 0;
-  if (s1 == NULL && s2 == NULL)
-    return 0;
-  else if (s1 == NULL)
-    return -1;
-  else if (s2 == NULL)
-    return 1;
-  
-  size_t i = 0;
-  while (i < n && s1[i] != '\0' && s2[i] != '\0') {
-    if (s1[i] < s2[i])
-      return -1;
-    else if (s1[i] > s2[i])
-      return 1;
-    i++;
+    
+  while (n--) {
+    if (*s1 == 0 || *s2 == 0 || *s1 != *s2) {
+      return *s1 - *s2;
+    }
+    s1++;
+    s2++;
   }
-
-  if (i < n) {
-    return 0;
-  }
-
-  if (s1[i] == '\0' && s2[i] == '\0')
-    return 0;
-  else if (s1[i] == '\0')
-    return -1;
-  else
-    return 1;
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
@@ -159,7 +142,6 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  return 0;
   char *tmp_s1 = (char *)s1;
   char *tmp_s2 = (char *)s2;
   return strncmp(tmp_s1, tmp_s2, n);
