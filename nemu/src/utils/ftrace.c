cce3da56ef12a8ca327528c2c330223b69b11bd3
tracer-ysyx <tracer@ysyx.org>
2023-11-10 20:27:17 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:27:17 up 11:48,  1 user,  load average: 0.04, 0.08, 0.06

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -70,7 +70,7 @@ void parse_elf(const char *elf_file) {
             break;
         }
     }
-
+printf("666\n");
     /* 读取符号表中的每个符号项 */ 
 
     fseek(fp, symtab_header.sh_offset, SEEK_SET);
@@ -79,7 +79,7 @@ void parse_elf(const char *elf_file) {
     size_t num_symbols = symtab_header.sh_size / symtab_header.sh_entsize;
     // 分配内存用于存储符号表
     symbol_tables = malloc(num_symbols * sizeof(symbol_table));
-printf("666\n");
+
     for (size_t i = 0; i < num_symbols; ++i) {
         if (fread(&symbol, sizeof(Elf64_Sym), 1, fp) <= 0) {
             fclose(fp);
