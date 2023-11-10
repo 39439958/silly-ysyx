d3f88a891cc7ffd92dfdf0c51626dd675f8dddbf
tracer-ysyx <tracer@ysyx.org>
2023-11-10 22:46:34 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:46:34 up 14:07,  1 user,  load average: 0.17, 0.07, 0.07

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -48,13 +48,10 @@ void parse_elf(const char *elf_file) {
             fclose(fp);
             exit(EXIT_FAILURE);
         }
-
         if (strtab_header.sh_type == SHT_STRTAB) {
             break;
         }
     }
-
-    printf("%u\n", strtab_header.sh_offset);
 
     // 读取字符串表内容
     char *string_table = malloc(strtab_header.sh_size);
@@ -63,7 +60,6 @@ void parse_elf(const char *elf_file) {
         fclose(fp);
         exit(EXIT_FAILURE);
     }
-
 
 
     // 寻找符号表节
