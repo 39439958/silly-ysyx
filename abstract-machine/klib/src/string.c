5a0a326d0f9f421377f3a3fb8c7db6cf145448fa
tracer-ysyx <tracer@ysyx.org>
2023-11-16 10:33:53 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:33:53 up  2:08,  1 user,  load average: 0.54, 0.13, 0.04

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,8 +5,6 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  if (s == NULL) 
-    return 0;
   size_t len = 0;
   while (*s++ != '\0') {
     len++;
@@ -15,123 +13,111 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  assert(dst != NULL && src != NULL);
-  char *res = dst;
-  while (*src != '\0') {
-    *dst++ = *src++;
-  }
-  *dst = '\0';
-  return res;
+  char *d = dst;
+  
+  while ((*d++ = *src++) != 0);
+
+  return d;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  assert(dst != NULL && src != NULL && n >= 0);
-  if (n == 0)
-    return dst;
-  char *res = dst;
-  while (*src != '\0' && n--) {
-    *dst++ = *src++;
+  char *d = dst;
+
+  while (n) {
+    if ((*d = *src) != 0) src++;
+    ++d;
+    --n;
   }
-  *dst = '\0';
-  return res;
+
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  assert(dst != NULL && src != NULL);
-  char *res = dst;
-  while (*dst != '\0') dst++;
-  while (*src != '\0') {
-    *dst++ = *src++;
-  } 
-  *dst = '\0';
-  return res;
+    char *d = dst;
+
+    while (*d++);
+    --d;
+    while ((*d++ = *src++) != 0);
+
+    return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  assert(s1 != NULL && s2 != NULL);
-  while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
-    s1++;
-    s2++;
+  while (*((char *)s1) == *((char *)s2)) {
+    if (!*s1++) {
+        return 0;
+    }
+    ++s2;
   }
-  return *s1 - *s2;
+
+  return (*((char *)s1) < *((char *)s2)) ? -1 : 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  assert(s1 != NULL && s2 != NULL && n >= 0);
-  if (n == 0)
-    return 0;
-    
-  while (n--) {
-    if (*s1 == 0 || *s2 == 0 || *s1 != *s2) {
-      return *s1 - *s2;
-    }
-    s1++;
-    s2++;
-  }
-  return 0;
+  while (n && (*((char *)s1) == *((char *)s2))) {
+		if (!*s1++) {
+			return 0;
+		}
+		++s2;
+		--n;
+	}
+
+	return (n == 0) ? 0 : (*((char *)s1) - *((char *)s2));
 }
 
 void *memset(void *s, int c, size_t n) {
-  if (s == NULL || n < 0)
-    return s;
-
   char *tmp = (char *)s;
 
   for (size_t i = 0; i < n; i++) {
     tmp[i] = c;
   }
+
   return s;
 }
 
+
 /**
- * 调用时请保证n大于str长度
+ * 此函数处理内存重叠情况
 */
 void *memmove(void *dst, const void *src, size_t n) {
-  if (dst == NULL || src == NULL)
-    return NULL;
-
-  if (dst > src && dst < (char *)src + n) {
-    // 内存重叠,需要从后往前复制
-    char *d = (char *)dst + n - 1;
-    char *s = (char *)src + n - 1;
-    
-    while (n--) {
-        *d-- = *s--;
-    }
-  } else {
-    // 内存不重叠,从前往后复制
     char *d = (char *)dst;
-    char *s = (char *)src;
-    
-    while (n--) {
-        *d++ = *s++;
+    const char *s = (const char *)src;
+
+    if (s >= d) {
+        while (n) {
+            *d++ = *s++;
+            --n;
+        }
+    } else {
+        while (n) {
+            --n;
+            d[n] = s[n];
+        }
     }
-  }
-  return dst;
+
+    return dst;
 }
 
 /**
  * 此函数不处理内存重叠，涉及内存重叠使用memmove
- * 调用时请保证n大于str长度
 */
 void *memcpy(void *out, const void *in, size_t n) {
-  if (out == NULL || in == NULL)
-    return NULL;
-  
-  char *d = (char *)out;
-  char *s = (char *)in;
+  char *o = (char *)out;
+  const char *i = (const char *)in;
 
   while (n--) {
-    *d++ = *s++;
+    *o++ = *i++;
   }
+
   return out;
 }
 
+/**
+ * 返回-1表示s1 < s2, 返回1表示s1 > s2, 返回0表示相等
+*/
 int memcmp(const void *s1, const void *s2, size_t n) {
-    if(s1 != NULL && s2 != NULL && n >= 0);
-
-    const unsigned char *p1 = s1;
-    const unsigned char *p2 = s2;
+    const char *p1 = s1;
+    const char *p2 = s2;
 
     for (size_t i = 0; i < n; i++) {
         if (p1[i] < p2[i]) {
