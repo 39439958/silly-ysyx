a2ddd4f4d133b970b1306734aa5a4f1647a051fa
tracer-ysyx <tracer@ysyx.org>
2023-11-23 15:02:13 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:13 up  4:12,  1 user,  load average: 0.30, 0.18, 0.06

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -109,7 +109,7 @@ int main(int argc, char** argv, char** env) {
           sim_time++;
           dut->clk ^= 1;
           dut->inst = pmem_read(dut->pc);
-          printf("pc : %x\n", dut->pc);
+          printf("pc : %x, inst : %x\n", dut->pc, dut->inst);
           dut->eval();
         }
         m_trace->dump(sim_time);
