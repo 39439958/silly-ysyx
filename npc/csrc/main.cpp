c8d5f9107b5d9bca687571e0fa4428a4db9b0b6c
tracer-ysyx <tracer@ysyx.org>
2023-11-28 11:29:13 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:13 up  2:12,  1 user,  load average: 0.27, 0.18, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -141,8 +141,13 @@ void npc_exec(int n) {
 
 void reg_display() {
     for (int i = 0; i < 32; i++) {
-    printf("%s : 0x%08x\n", regs[i], top->rootp->top__DOT__exu0__DOT__regfile0__DOT__rf[i]);
-  }
+        printf("%s  : 0x%08x", regs[i], top->rootp->top__DOT__exu0__DOT__regfile0__DOT__rf[i]);
+        if (i % 4 == 3) {
+            printf("\n");
+        } else {
+            printf("\t");
+        }   
+    }
 }
 
 static int cmd_c(char *args) {
