11a1222c4707270b1ca0e6a21a75cbef024a0ab4
tracer-ysyx <tracer@ysyx.org>
2023-11-28 10:40:31 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:40:31 up  1:24,  1 user,  load average: 0.17, 0.14, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -9,109 +9,52 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+#define NPC_QUIT 0
+#define NPC_ABORT 1
 
-#define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
-int exited = 0;
-int err = 0;
+int is_quit = 0;
+int quit_state = 0;
+
 static uint8_t pmem[0x8000000] __attribute((aligned(4096))) = {};
 char *img_file = NULL;
 static const uint32_t img [] = {
-  0x00108093,
-  0x00108093,
-  0x00108093,
-  0x00108093,
-  0x00100073,
+    0x00108093,
+    0x00108093,
+    0x00108093,
+    0x00108093,
+    0x00100073,
 };
+
+Vtop *top = new Vtop;
+VerilatedVcdC *m_trace = new VerilatedVcdC;
+
+void ebreak() {
+    is_quit = 1;
+}
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
-  static char *line_read = NULL;
+    static char *line_read = NULL;
 
-  if (line_read) {
-    free(line_read);
-    line_read = NULL;
-  }
-
-  line_read = readline("(npc) ");
-
-  if (line_read && *line_read) {
-    add_history(line_read);
-  }
-
-  return line_read;
-}
-
-static int cmd_c(char *args) {
-  cpu_exec(-1);
-  return 0;
-}
-
-
-static int cmd_q(char *args) {
-  nemu_state.state = NEMU_QUIT;
-  return -1;
-}
-
-static int cmd_help(char *args);
-
-static int cmd_si(char *args);
-
-static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NPC", cmd_q },
-  { "si", "Step through N instructions", cmd_si},
-};
-
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
-
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    if (line_read) {
+        free(line_read);
+        line_read = NULL;
     }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
+
+    line_read = readline("(npc) ");
+
+    if (line_read && *line_read) {
+        add_history(line_read);
     }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
-}
 
-static int cmd_si(char *args) {
-  int n = 0;
-  if (args == NULL)
-    n = 1;
-  else
-    sscanf(args, "%d", &n);
-  cpu_exec(n);
-  return 0;
-}
-
-void ebreak() {
-  exited = 1;
-}
-
-uint32_t pmem_read(uint32_t pc){
-    return *(uint32_t *)(pmem + pc - 0x80000000);
+    return line_read;
 }
 
 void init_mem() {
-  memset(pmem, rand(), 0x8000000);
-  memcpy(pmem, img, sizeof(img));
+    memset(pmem, rand(), 0x8000000);
+    memcpy(pmem, img, sizeof(img));
 }
 
 void parse_img(int argc, char** argv) {
@@ -144,53 +87,157 @@ void load_img() {
     fclose(fp);
 }
 
+void npc_rst() {
+    top->clk = 0;
+    top->rst = 1;
+    top->eval();
+    m_trace->dump(sim_time);
+    sim_time++;
 
-int main(int argc, char** argv, char** env) {
-    Vtop *top = new Vtop;
+    top->clk = 1;
+    top->rst = 1;
+    top->eval();
+    m_trace->dump(sim_time);
+    sim_time++;
+}
 
-    Verilated::traceEverOn(true);
-    VerilatedVcdC *m_trace = new VerilatedVcdC;
-    top->trace(m_trace, 5);
-    m_trace->open("waveform.vcd");
+uint32_t pmem_read(uint32_t pc){
+    return *(uint32_t *)(pmem + pc - 0x80000000);
+}
 
-    init_mem();
-
-    parse_img(argc, argv);
-
-    load_img();
-
-    while (!exited) {
-        
-        if (sim_time == 0) {
-			    top->clk = 0;
-			    top->rst = 1;
-          top->eval();
-          m_trace->dump(sim_time);
-          sim_time++;
-          top->clk = 1;
-          top->rst = 1;
-          top->eval();
-		    } 
-        else {
-          top->clk ^= 1;
-          top->rst = 0;
-          top->eval();
-          m_trace->dump(sim_time);
-          sim_time++;
-          top->clk ^= 1;
-          top->inst = pmem_read(top->pc);
-          printf("pc : %x, inst : %08x\n", top->pc, top->inst);
-          if (top->inst == 0x0000006f) {
-            exited = 1;
-            err = 1;
-          }
-          top->eval();
-        }
+void npc_exec(int n) {
+    while (n--) { 
+        top->clk ^= 1;
+        top->rst = 0;
+        top->eval();
         m_trace->dump(sim_time);
         sim_time++;
+
+        top->clk ^= 1;
+        top->inst = pmem_read(top->pc);
+        top->eval();
+        m_trace->dump(sim_time);
+        sim_time++;
+
+        if (top->inst == 0x0000006f) {
+            is_quit = 1;
+            quit_state = NPC_ABORT;
+        }
+        printf("pc : %x, inst : %08x\n", top->pc, top->inst);
+
+        if (is_quit) {
+            break;
+        }
     }
-    err ? printf("\33[1;41mHIT BAD TRAP\33[0m\n") : printf("\33[1;32mHIT GOOD TRAP\33[0m\n");
+}
+
+static int cmd_c(char *args) {
+    npc_exec(-1);
+    return is_quit;
+}
+
+static int cmd_q(char *args) {
+    is_quit = 1;
+    return 1;
+}
+
+static int cmd_help(char *args);
+
+static int cmd_si(char *args);
+
+static struct {
+    const char *name;
+    const char *description;
+    int (*handler) (char *);
+} cmd_table [] = {
+    { "help", "Display information about all supported commands", cmd_help },
+    { "c", "Continue the execution of the program", cmd_c },
+    { "q", "Exit NPC", cmd_q },
+    { "si", "Step through N instructions", cmd_si},
+};
+
+static int cmd_help(char *args) {
+    /* extract the first argument */
+    char *arg = strtok(NULL, " ");
+    int i;
+
+    if (arg == NULL) {
+        /* no argument given */
+        for (i = 0; i < NR_CMD; i ++) {
+            printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        }
+    }
+    else {
+        for (i = 0; i < NR_CMD; i ++) {
+            if (strcmp(arg, cmd_table[i].name) == 0) {
+                printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+                return 0;
+            }
+        }
+        printf("Unknown command '%s'\n", arg);
+    }
+    return 0;
+}
+
+static int cmd_si(char *args) {
+    int n = 0;
+    if (args == NULL)
+        n = 1;
+    else
+        sscanf(args, "%d", &n);
+    npc_exec(n);
+    return is_quit;
+}
+
+void exit_work() {
+    quit_state ? printf("\33[1;41mHIT BAD TRAP\33[0m\n") : printf("\33[1;32mHIT GOOD TRAP\33[0m\n");
     m_trace->close();
     delete top;
     exit(EXIT_SUCCESS);
+}
+
+
+int main(int argc, char** argv, char** env) {
+    // start waveform trace
+    Verilated::traceEverOn(true);
+    top->trace(m_trace, 5);
+    m_trace->open("waveform.vcd");
+
+    // initial memory and instruction
+    init_mem();
+    parse_img(argc, argv);
+    load_img();
+
+    // initial NPC rst
+    npc_rst();
+
+    for (char *str; (str = rl_gets()) != NULL; ) {
+        char *str_end = str + strlen(str);
+
+        /* extract the first token as the command */
+        char *cmd = strtok(str, " ");
+        if (cmd == NULL) { 
+            continue; 
+        }
+
+        /* treat the remaining string as the arguments,
+        * which may need further parsing
+        */
+        char *args = cmd + strlen(cmd) + 1;
+        if (args >= str_end) {
+            args = NULL;
+        }
+
+        int i;
+        for (i = 0; i < NR_CMD; i ++) {
+        if (strcmp(cmd, cmd_table[i].name) == 0) {
+            if (cmd_table[i].handler(args) != 0) { 
+                exit_work();
+            }
+            break;
+        }
+        }
+
+        if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    }
 }
