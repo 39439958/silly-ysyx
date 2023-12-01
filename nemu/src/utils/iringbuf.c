84ad0a7ea1a62c5a59ea0450c664ff5998273598
tracer-ysyx <tracer@ysyx.org>
2023-12-01 10:16:46 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:16:46 up 52 min,  1 user,  load average: 0.30, 0.11, 0.08

--- nemu/src/utils/iringbuf.c
+++ nemu/src/utils/iringbuf.c
@@ -3,20 +3,20 @@
 
 typedef struct iringbuf
 {
-  vaddr_t pcs[20];
-  uint32_t insts[20];
-  uint32_t iring_rf;
-  uint32_t iring_wf;
+    vaddr_t pcs[20];
+    uint32_t insts[20];
+    uint32_t iring_rf;
+    uint32_t iring_wf;
 }iringbuf;
 
 iringbuf irb;
 
 void iringbuf_write_inst(vaddr_t pc, uint32_t inst) {
-  irb.pcs[irb.iring_wf] = pc;
-  irb.insts[irb.iring_wf] = inst;
-  irb.iring_wf = (irb.iring_wf + 1) % 20;
-  if (irb.iring_wf == irb.iring_rf)
-    irb.iring_rf = (irb.iring_rf + 1) % 20;
+    irb.pcs[irb.iring_wf] = pc;
+    irb.insts[irb.iring_wf] = inst;
+    irb.iring_wf = (irb.iring_wf + 1) % 20;
+    if (irb.iring_wf == irb.iring_rf)
+        irb.iring_rf = (irb.iring_rf + 1) % 20;
 }
 
 void iringbuf_display() {
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -134,10 +134,17 @@ void npc_exec(int n) {
         }
 
         // print instruction
-        // void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-        // disassemble(inst_buf, sizeof(inst_buf), top->pc, (uint8_t *)&top->inst, 4);
-        // disassemble(inst_buf, sizeof(inst_buf), top->pc, (uint8_t *)&top->inst, 4);
-        printf("pc : 0x%x, inst : 0x%08x\n", top->pc, top->inst);
+        char *p = inst_buf;
+        uint32_t pc = top->pc;
+        uint8_t *inst = (uint8_t *)&top->inst;
+        p += snprintf(p, sizeof(inst_buf), "0x%08:", pc);
+        for (int j = 3; j >= 0; j--) {
+            p += snprintf(p, 4, " %02x", inst[j]);
+        }
+        memset(p, ' ', 4);
+        p += 4;
+        void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+        //disassemble(p, inst_buf + sizeof(inst_buf) - p, pc, inst, 4);
         printf("%s\n", inst_buf);
 
         if (is_quit) {
