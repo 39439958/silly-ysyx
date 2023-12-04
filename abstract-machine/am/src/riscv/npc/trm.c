e88eb996c83705b3105d6f6d6f9482fed6572c4a
tracer-ysyx <tracer@ysyx.org>
2023-12-04 12:43:50 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:43:50 up  3:37,  1 user,  load average: 0.25, 0.35, 0.21

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -7,7 +7,7 @@ int main(const char *args);
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-# define ebreak(code) asm volatile ("move $v0, %0; sdbbp" : :"r"(code))
+# define ebreak(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
