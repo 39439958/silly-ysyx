51596f85ce1e0711e5a4523ec8c940fba0ec78a9
silly <li_xu@mail.ustc.edu.cn>
2023-12-04 11:40:45 +0800

    nemu start mario

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,9 +1,59 @@
+// #include <am.h>
+// #include <klib.h>
+// #include <klib-macros.h>
+
+// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+// static unsigned long int next = 1;
+
+// int rand(void) {
+//   // RAND_MAX assumed to be 32767
+//   next = next * 1103515245 + 12345;
+//   return (unsigned int)(next/65536) % 32768;
+// }
+
+// void srand(unsigned int seed) {
+//   next = seed;
+// }
+
+// int abs(int x) {
+//   return (x < 0 ? -x : x);
+// }
+
+// int atoi(const char* nptr) {
+//   int x = 0;
+//   while (*nptr == ' ') { nptr ++; }
+//   while (*nptr >= '0' && *nptr <= '9') {
+//     x = x * 10 + *nptr - '0';
+//     nptr ++;
+//   }
+//   return x;
+// }
+
+// void *malloc(size_t size) {
+//   // On native, malloc() will be called during initializaion of C runtime.
+//   // Therefore do not call panic() here, else it will yield a dead recursion:
+//   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   panic("Not implemented");
+// #endif
+//   return NULL;
+// }
+
+// void free(void *ptr) {
+// }
+
+// #endif
+
+
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+
+static char *malloc_addr;
+static bool is_initial = false;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -33,10 +83,18 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+    if (!is_initial) {
+        malloc_addr = (void *)ROUNDUP(heap.start, 8);
+        is_initial = true;
+    }
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+    size = (size_t)ROUNDUP(size, 8);
+    char *old = malloc_addr;
+    malloc_addr += size;
+    //assert((uintptr_t)heap.start <= (uintptr_t)malloc_addr && (uintptr_t)malloc_addr < (uintptr_t)heap.end);
+    return old;
 #endif
-  return NULL;
+    return NULL;
 }
 
 void free(void *ptr) {
