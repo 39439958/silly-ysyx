2bc3b87abfb86891f3875247165582a79a07a23e
tracer-ysyx <tracer@ysyx.org>
2023-12-04 22:29:37 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:29:37 up 13:23,  1 user,  load average: 0.19, 0.10, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -273,6 +273,14 @@ void npc_exec(int n) {
         top->rst = 0;
         top->eval();
 
+        top->clk ^= 1;
+        top->rst = 0;
+        top->eval();
+
+        top->clk ^= 1;
+        top->rst = 0;
+        top->eval();
+
         printf("%x\n", top->rootp->top__DOT__exu0__DOT__regfile0__DOT__rf[1]);
 
         // difftest
