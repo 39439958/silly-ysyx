7238d1e715937efbfc28d81ae9aa15f656906808
tracer-ysyx <tracer@ysyx.org>
2023-12-17 10:04:00 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:04:00 up  1:31,  1 user,  load average: 0.11, 0.11, 0.13

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,13 +10,13 @@ Context* __am_irq_handle(Context *c) {
     switch (c->mcause) {
       default: ev.event = EVENT_ERROR; break;
     }
-    printf("mepc: %x\n", c->mepc);
-    printf("mstatus: %x\n", c->mstatus);
-    printf("mcause: %x\n", c->mcause);
-    for (int i = 0; i < 32; i++) {
-      printf("x%d: %x ", i, c->gpr[i]);
-    }
-    
+    printf("mepc: %lx\n", c->mepc);
+    // printf("mstatus: %x\n", c->mstatus);
+    // printf("mcause: %x\n", c->mcause);
+    // for (int i = 0; i < 32; i++) {
+    //   printf("x%d: %x ", i, c->gpr[i]);
+    // }
+
     c = user_handler(ev, c);
     assert(c != NULL);
   }
