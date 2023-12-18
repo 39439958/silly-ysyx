79282f4eb380b166ad0f1905ae191878a6813c73
tracer-ysyx <tracer@ysyx.org>
2023-12-18 18:00:58 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:00:58 up  8:54,  1 user,  load average: 0.26, 0.10, 0.08

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,7 +37,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       memset((void *)(ph[i].p_vaddr + ph[i].p_filesz), 0, ph[i].p_memsz - ph[i].p_filesz);
     }
   }
-  Log("vaddr: %p\n",(void *)ph[0].p_vaddr);
+  Log("vaddr: %p\n",(void *)ph[1].p_vaddr);
   return ehdr.e_entry;
 }
 
