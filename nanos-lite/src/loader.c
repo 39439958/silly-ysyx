a6bef90a15b4ea0ef142d6b6f2d98422c045a8df
tracer-ysyx <tracer@ysyx.org>
2023-12-18 14:27:45 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:45 up  5:21,  1 user,  load average: 0.18, 0.07, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,24 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // open elf
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+
+  // read phdr
+  Elf_Phdr ph[ehdr.e_phnum];
+  ramdisk_read(ph, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (ph[i].p_type == PT_LOAD) {
+      ramdisk_read((void *)ph[i].p_vaddr, ph[i].p_offset, ph[i].p_memsz);
+      memset((void *)(ph[i].p_vaddr + ph[i].p_filesz), 0, ph[i].p_memsz - ph[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
