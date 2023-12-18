2b77c25011cb1a3e398daaa0349a9fadd5bf2545
tracer-ysyx <tracer@ysyx.org>
2023-12-18 14:35:20 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:35:20 up  5:28,  1 user,  load average: 0.16, 0.08, 0.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,6 +9,14 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__riscv)
+# define EXPECT_TYPE EM_RISCV  // see /usr/include/elf.h to get the right type
+#else 
+# error Unsupported ISA
+#endif
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
@@ -16,6 +24,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+
+  // check elf type
+  assert(ehdr.e_machine == EXPECT_TYPE);
 
   // read phdr
   Elf_Phdr ph[ehdr.e_phnum];
