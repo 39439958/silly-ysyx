a43e25e4728c1fec101afd67be11d055528bc24c
tracer-ysyx <tracer@ysyx.org>
2023-12-18 16:10:53 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:10:53 up  7:04,  1 user,  load average: 0.06, 0.09, 0.05

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,7 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_NULL : do_syscall(c); break;
     case EVENT_YIELD : Log("yield succeed!"); break;
     case EVENT_SYSCALL : do_syscall(c); break;
     default: panic("irq:Unhandled event ID = %d", e.event);
