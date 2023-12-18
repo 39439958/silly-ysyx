bc4097247a8b6dc0b8fa460001518543e9375137
tracer-ysyx <tracer@ysyx.org>
2023-12-18 16:40:28 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:40:28 up  7:34,  1 user,  load average: 0.05, 0.08, 0.04

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,10 +16,18 @@ void sys_exit(int code) {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
+  uintptr_t a0 = a[1];
 
   switch (a[0]) {
     case SYS_exit: sys_exit(a[0]); break;
-    case SYS_yield: a[0] = sys_yield(); break;
+    case SYS_yield: c->GPRx = sys_yield(); break;
     default: panic("syscall:Unhandled syscall ID = %d", a[0]);
   }
+
+  // strace
+  Log("strace : %s , a0 : %d, a1 : %d, a2 : %d, ret : %d\n", syscall[a[0]], a0, a[2], a[3], c->GPRx);
 }
