5c5427267e92e31bdda132d9bbfdf80419eabc29
tracer-ysyx <tracer@ysyx.org>
2023-12-19 12:48:00 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:48:00 up  3:30,  1 user,  load average: 0.02, 0.09, 0.04

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -76,6 +76,7 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
+  _write(1, "sbrk\n", 5);
   intptr_t addr = program_break + increment;
   if (addr >= (intptr_t)&_end) {
     if (_syscall_(SYS_brk, addr, 0, 0) == 0) {
