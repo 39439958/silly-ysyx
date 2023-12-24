b35b18539663089fde70e6e9032ad91fa7fa3d91
tracer-ysyx <tracer@ysyx.org>
2023-12-24 18:28:19 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:28:19 up  9:47,  1 user,  load average: 0.27, 0.13, 0.04

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -63,7 +63,7 @@ void ret_trace(paddr_t pc);
 
 static void etrace() {
   IFDEF(CONFIG_ETRACE, {
-    printf("\n" ANSI_FMT("[ETRACE]", ANSI_FG_RED) "ecall in mepc = " FMT_WORD ", mcause = %d\n", cpu.csrs.mepc, cpu.csrs.mcause);
+    printf("\n" ANSI_FMT("[ETRACE]", ANSI_FG_BLUE) "ecall in mepc = " FMT_WORD ", mcause = %d\n", cpu.csrs.mepc, cpu.csrs.mcause);
   });
 }
 
