3a1923b380d527e2f4bcfde09d7a3534501f3d15
tracer-ysyx <tracer@ysyx.org>
2023-12-24 14:40:38 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:38 up  6:00,  1 user,  load average: 0.57, 0.24, 0.13

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,8 +43,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (examine_watchpoint(_this->pc))
     nemu_state.state = NEMU_STOP;
   // breakpoint
-   if (_this->pc == 0x80001610)
-    nemu_state.state = NEMU_STOP;  
+  //  if (_this->pc == 0x80001610)
+  //   nemu_state.state = NEMU_STOP;  
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
