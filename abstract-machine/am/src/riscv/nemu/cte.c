990910f21d0901bc44ac6c37265a428e84d1d0e5
tracer-ysyx <tracer@ysyx.org>
2023-12-24 20:34:44 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:44 up 11:54,  1 user,  load average: 0.12, 0.13, 0.09

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,11 +9,8 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0: ev.event = EVENT_SYSCALL; break;
+      case 0: case 1: case 2: case 3: case 4: case 7: case 8: case 9: ev.event = EVENT_SYSCALL; break;
       case 11: ev.event = EVENT_YIELD; break;
-      case 1: ev.event = EVENT_SYSCALL; break;
-      case 4: ev.event = EVENT_SYSCALL; break;
-      case 9: ev.event = EVENT_SYSCALL; break;
       default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
