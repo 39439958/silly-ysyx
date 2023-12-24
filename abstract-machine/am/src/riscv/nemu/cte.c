4206e0308101397f870e03a7f9bd78cfb6148738
tracer-ysyx <tracer@ysyx.org>
2023-12-24 15:08:48 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:48 up  6:28,  1 user,  load average: 0.18, 0.11, 0.10

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,7 +10,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 0: ev.event = EVENT_SYSCALL; break;
-      case -1: ev.event = EVENT_YIELD; break;
+      case 11: ev.event = EVENT_YIELD; break;
       case 1: ev.event = EVENT_SYSCALL; break;
       case 4: ev.event = EVENT_SYSCALL; break;
       case 9: ev.event = EVENT_SYSCALL; break;
@@ -42,7 +42,7 @@ void yield() {
 #ifdef __riscv_e
   asm volatile("li a5, -1; ecall");
 #else
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, 11; ecall");
 #endif
 }
 
