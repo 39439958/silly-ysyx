170f3a30e94f509243e7edcc9edcec8629aaf7d1
tracer-ysyx <tracer@ysyx.org>
2023-12-24 16:14:48 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:48 up  7:34,  1 user,  load average: 0.22, 0.11, 0.03

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,11 +20,12 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   // mstatus set
+  printf("[Before ECALL] mstatus = %x\n", cpu.csrs.mstatus);
   cpu.csrs.mstatus &= ~(1<<7);
   cpu.csrs.mstatus |= ((cpu.csrs.mstatus&(1<<3))<<4);
   cpu.csrs.mstatus &= ~(1<<3);
   cpu.csrs.mstatus |= ((1<<11)+(1<<12));
-  
+
   // store pc in mepc
   cpu.csrs.mepc = epc; 
   // set err number in mcause
