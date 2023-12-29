d167815c640fdd5148f27fdc4cc03b7c5dea7c7d
tracer-ysyx <tracer@ysyx.org>
2023-12-29 15:25:05 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:25:05 up  4:51,  1 user,  load average: 0.16, 0.17, 0.09

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -5,7 +5,7 @@ int main() {
   gettimeofday(&tv, NULL);
   printf("ms = %d\n", tv.tv_sec * 1000);
 
-  __uint64_t ms = 500;
+  int ms = 500;
   // while (1) {
   //   while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
   //     gettimeofday(&tv, NULL);
