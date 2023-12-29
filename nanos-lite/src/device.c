fd13a1432dbc801c00c83ede9d4e2b3ead28c6b0
tracer-ysyx <tracer@ysyx.org>
2023-12-29 20:26:57 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:57 up  9:53,  1 user,  load average: 0.21, 0.17, 0.10

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,7 +26,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T kbd;
   ioe_read(AM_INPUT_KEYBRD, &kbd);
   if (kbd.keycode != AM_KEY_NONE) {
-    int ret = snprintf((char *)buf, len, "%s %s\n", kbd.keydown ? "kd" : "ku", keyname[kbd.keycode]);
+    int ret = snprintf((char *)buf, len, "%s %s\n\0", kbd.keydown ? "kd" : "ku", keyname[kbd.keycode]);
     printf("%s\n", buf);
     return ret;
   }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -18,7 +18,8 @@ typedef struct {
   size_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENT};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT};
+//FD_FB
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("Should not reach here");
