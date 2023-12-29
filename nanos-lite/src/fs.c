5c88292daa2567fdfbe0169081d4b34b2252beeb
tracer-ysyx <tracer@ysyx.org>
2023-12-29 20:11:55 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:11:55 up  9:38,  1 user,  load average: 0.30, 0.19, 0.09

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,13 +41,13 @@ static Finfo file_table[] __attribute__((used)) = {
 
 int fs_open(const char *pathname, int flags, int mode) {
   int ft_len = sizeof(file_table) / sizeof(Finfo);
-  printf("666\n");
   for (int i = 3; i < ft_len; i++) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
       return i;
     }
   }
+  printf("666\n");
   panic("Should not reach here");
   return 0;
 }
