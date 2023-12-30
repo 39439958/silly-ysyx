91aab28c2f8e4ccb05b9236be72d50d3e6794a48
tracer-ysyx <tracer@ysyx.org>
2023-12-30 19:38:22 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:38:22 up  9:30,  1 user,  load average: 0.13, 0.08, 0.02

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -48,7 +48,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   AM_GPU_FBDRAW_T fb_ctl;
   ioe_read(AM_GPU_FBDRAW, &fb_ctl);
-  
+  printf("666\n");
   strcpy(fb_ctl.pixels, buf);
   fb_ctl.x = offset / screen_w;
   fb_ctl.y = offset % screen_w;
