f285512a5a90582f7f93e586931e3e4e84efb7ac
tracer-ysyx <tracer@ysyx.org>
2023-12-30 16:22:21 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:22:21 up  6:14,  1 user,  load average: 0.00, 0.06, 0.08

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 static int screen_w = 0, screen_h = 0;
-AM_GPU_FBDRAW_T ctl;
+AM_GPU_FBDRAW_T fb_ctl;
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   char *cbuf = (char *)buf;
@@ -47,18 +47,16 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  strncpy(ctl.pixels, buf, len);
+  strncpy(fb_ctl.pixels, buf, len);
 
-  ctl.x = offset / screen_w;
+  fb_ctl.x = offset / screen_w;
+  fb_ctl.y = offset % screen_w;
+  fb_ctl.w = len, fb_ctl.h = 1;
+  fb_ctl.sync = true;
 
-  ctl.y = offset % screen_w;
+  ioe_write(AM_GPU_FBDRAW, &fb_ctl);
 
-  ctl.w = len, ctl.h = 1;
-
-  ctl.sync = true;
-  printf("66\n");
-  ioe_write(AM_GPU_FBDRAW, &ctl);
-   return 0;
+  return 0;
 }
 
 void init_device() {
