78d511947937b0260e6ab98c1a0683f0b896657e
tracer-ysyx <tracer@ysyx.org>
2024-01-01 12:41:18 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:41:18 up  3:41,  1 user,  load average: 0.42, 0.15, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -66,7 +66,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  int *tmp = (int *)s;
+  char *tmp = (char *)s;
 
   for (size_t i = 0; i < n; i++) {
     tmp[i] = c;
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -17,7 +17,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   if (dstrect == NULL) {
     for(int i = 0; i < dst->h; i++) {
-      memset((uint32_t *)dst->pixels + (i * dst->w), color, dst->w);
+      for (int j = 0; j < dst->w; j++) {
+        uint32_t *pixels = (uint32_t *)(dst->pixels + (i * dst->w) + j);
+        *pixels = color;
+      }
     }
   } else {
     for(int i = 0; i < dstrect->h; i++) {
