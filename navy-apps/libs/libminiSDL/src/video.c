04dfb02ce7d9e9c4c0ec04505bab554704d1d61a
tracer-ysyx <tracer@ysyx.org>
2024-01-01 11:20:28 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:20:28 up  2:20,  1 user,  load average: 0.76, 0.37, 0.20

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -14,9 +14,15 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  // for(int i = 0; i < dstrect->h; i++) {
-  //   memset(dst->pixels + (dstrect->y * dst->w + dstrect->x) + (i * dst->w), color, dstrect->w);
-  // }
+  if (dstrect == NULL) {
+    for(int i = 0; i < dst->h; i++) {
+      memset(dst->pixels + (i * dst->w), color, dst->w);
+    }
+  } else {
+    for(int i = 0; i < dstrect->h; i++) {
+      memset(dst->pixels + (dstrect->y * dst->w + dstrect->x) + (i * dst->w), color, dstrect->w);
+    }
+  }
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
