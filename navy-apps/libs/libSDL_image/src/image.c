e8867c6254badbbdd6885216ae53eba2a8778c4d
tracer-ysyx <tracer@ysyx.org>
2024-01-01 23:38:22 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:38:22 up  2:32,  1 user,  load average: 0.15, 0.24, 0.21

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,41 +12,34 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  int fd = open(filename, 0, 0);
-  int size = lseek(fd, 0, SEEK_END);
-  void *buf = malloc(size);
-  lseek(fd, 0, SEEK_SET);
-  read(fd, buf, size);
-  SDL_Surface *surface = STBIMG_LoadFromMemory((char *)buf, size);
-  free(buf);
-  close(fd);
-  return surface;
-  // FILE *fp = fopen(filename, "r");
-  // if (!fp) return NULL;
-
-  // long file_size = 0;
-  // long cur_offset = ftell(fp);
-  // if (cur_offset == -1) {
-  //   return NULL;
-  // }
-  // if (fseek(fp, 0, SEEK_END) != 0) {
-  //   return NULL;
-  // }
-  // file_size = ftell(fp);
-  // if (file_size == -1) {
-  //   return NULL;
-  // }
-  // if (fseek(fp, cur_offset, SEEK_SET) != 0) {
-  //   return NULL;
-  // }
-  
-  // char buf[file_size];
-  // fread(buf, 1, file_size, fp);
-  // SDL_Surface *ret = STBIMG_LoadFromMemory(buf, file_size);
-
+  // int fd = open(filename, 0, 0);
+  // int size = lseek(fd, 0, SEEK_END);
+  // void *buf = malloc(size);
+  // lseek(fd, 0, SEEK_SET);
+  // read(fd, buf, size);
+  // SDL_Surface *surface = STBIMG_LoadFromMemory((char *)buf, size);
   // free(buf);
-  // fclose(fp);
-  // return NULL;
+  // close(fd);
+  // return surface;
+  FILE *fp = fopen(filename, "r");
+  if (!fp) return NULL;
+
+  long file_size = 0;
+  long cur_offset = ftell(fp);
+
+  fseek(fp, 0, SEEK_END);
+
+  file_size = ftell(fp);
+
+  fseek(fp, cur_offset, SEEK_SET);
+  
+  char buf[file_size];
+  fread(buf, 1, file_size, fp);
+  SDL_Surface *ret = STBIMG_LoadFromMemory((char *)buf, file_size);
+
+  free(buf);
+  fclose(fp);
+  return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
