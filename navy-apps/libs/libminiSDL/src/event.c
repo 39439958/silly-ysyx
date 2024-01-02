e09a12da38c8352419bfb0d188a07ef8504f9418
tracer-ysyx <tracer@ysyx.org>
2024-01-02 21:39:02 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:39:02 up 10:41,  1 user,  load average: 0.17, 0.18, 0.18

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -16,40 +16,33 @@ int SDL_PushEvent(SDL_Event *ev) {
 
 int SDL_PollEvent(SDL_Event *ev) {
   char buf[64];
+  char type[8], key_name[8];
+  int keycode;
+
   if (NDL_PollEvent(buf, 0) == 0) {
     return 0;
   }
-  char type[8], key_name[8];
-  int keycode;
   sscanf(buf, "%s %s %d\n", type, key_name, &keycode);
-  if (strcmp(type, "kd") == 0) {
-    ev->type = SDL_KEYDOWN;
-    ev->key.keysym.sym = keycode;
-    // key_state[keycode] = 1;
-  } else {
-    ev->type = SDL_KEYUP;
-    ev->key.keysym.sym = keycode;
-    // key_state[keycode] = 0;
-  }
-  return 1;
+  ev->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+  ev->key.keysym.sym = keycode;
+  return 1; 
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
-  NDL_PollEvent(buf, 0);
   char type[8], key_name[8];
   int keycode;
-  sscanf(buf, "%s %s %d\n", type, key_name, &keycode);
-  if (strcmp(type, "kd") == 0) {
-    event->type = SDL_KEYDOWN;
+
+  while (1) {
+    if (NDL_PollEvent(buf, sizeof(buf)) == 0) {
+      continue;
+    }
+    sscanf(buf, "%s %s %d\n", type, key_name, &keycode);
+    event->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
     event->key.keysym.sym = keycode;
-    // key_state[keycode] = 1;
-  } else {
-    event->type = SDL_KEYUP;
-    event->key.keysym.sym = keycode;
-    // key_state[keycode] = 0;
+    return 1;
   }
-  return 1;
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
