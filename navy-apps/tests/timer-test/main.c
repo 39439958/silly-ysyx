a5bd66e10f3515dc9f6c5b31a06c4f0c3adadf10
silly <li_xu@mail.ustc.edu.cn>
2024-01-05 21:47:33 +0800

    time-test

---
+++ navy-apps/tests/timer-test/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <NDL.h>
+
+
+int main() {
+  NDL_Init(0);
+
+  uint32_t now_time = NDL_GetTicks();
+  uint32_t ms = 500;
+  while (ms <= 10000) {
+    while ((now_time) < ms) {
+      now_time = NDL_GetTicks();
+    }
+    printf("ms = %d\n", ms);
+    ms += 500;
+  }
+  NDL_Quit();
+}
