c881efa9753b9560c482e94b32c272b364f2f8a0
tracer-ysyx <tracer@ysyx.org>
2024-01-05 16:05:27 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:05:27 up  1:15,  1 user,  load average: 0.54, 0.20, 0.20

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -26,7 +26,8 @@ static void sh_handle_cmd(const char *cmd) {
   char tmp_cmd[64];
   strcpy(tmp_cmd, cmd);
   tmp_cmd[strlen(cmd) - 1] = '\0';
-  execve(tmp_cmd, NULL, NULL);
+  setenv("PATH", "/bin", 0);
+  execvp(tmp_cmd, NULL);
 }
 
 void builtin_sh_run() {
