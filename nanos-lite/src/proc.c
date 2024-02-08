9085c474e41f7c409577b6b9bf57472a3132f17c
tracer-ysyx <tracer@ysyx.org>
2024-02-08 13:46:01 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:46:01 up 14:32,  1 user,  load average: 0.15, 0.10, 0.03

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,6 +33,8 @@ void context_uload(PCB *p, const char *filename, char *const argv[], char *const
   while (argv[argc] != NULL) argc++;
   while (envp[envc] != NULL) envc++;
 
+  printf("666\n");
+
   char* us1 = (char*)heap.end;
   // clone argv
   for (int i = 0; i < argc; i++) {
