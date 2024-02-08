4172d9e257e95b903f2bd02161fddf96ccfed2d2
tracer-ysyx <tracer@ysyx.org>
2024-02-09 00:23:26 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:23:26 up 1 day,  1:09,  1 user,  load average: 0.09, 0.13, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -64,9 +64,9 @@ int sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
   if (pathname == NULL)
     return -1;
   naive_uload(NULL, pathname);
-  // context_uload(current, pathname, argv, envp);
-  // switch_boot_pcb();
-  // yield();
+  context_uload(current, pathname, argv, envp);
+  switch_boot_pcb();
+  yield();
   return 0;
 }
 
