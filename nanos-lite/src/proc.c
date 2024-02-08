53bd9759e737f6d5b5e2053c3386800929f0e166
tracer-ysyx <tracer@ysyx.org>
2024-02-09 00:44:44 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:44:44 up 1 day,  1:30,  1 user,  load average: 0.34, 0.21, 0.13

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -75,9 +75,9 @@ void init_proc() {
   // context_kload(&pcb[0], hello_fun, (void *)1L);
   // context_kload(&pcb[1], hello_fun, (void *)2L);
 
-  char *argv[] = {"--skip"}; 
+  char *argv[] = {"/bin/exec-test"}; 
   char *envp[] = {NULL}; 
-  context_uload(&pcb[0], "/bin/pal", argv, envp);
+  context_uload(&pcb[0], "/bin/exec-test", argv, envp);
   switch_boot_pcb();
 
   Log("Initializing processes...");
