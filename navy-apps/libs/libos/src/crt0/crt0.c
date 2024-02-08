a3e089963bcb092a7e2e11308dd4d764f0ae5b8b
tracer-ysyx <tracer@ysyx.org>
2024-02-08 09:45:58 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:45:58 up 10:31,  1 user,  load average: 0.54, 0.28, 0.12

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,13 +5,14 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  int argc = (int)args[0];
+  // int argc = (int)args[0];
 
-  char **argv = (char **)(args + 1);
+  // char **argv = (char **)(args + 1);
 
-  char **envp = (char **)(args + argc + 2);
-  
-  environ = envp;
-  exit(main(argc, argv, envp));
+  // char **envp = (char **)(args + argc + 2);
+
+  char *empty[] = {NULL};
+  environ = empty;
+  exit(main(0, empty, empty));
   assert(0);
 }
