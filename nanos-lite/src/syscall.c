7f4d5bfdc3ed51d6bef76a5c80d5af5720326ee9
tracer-ysyx <tracer@ysyx.org>
2024-02-09 00:17:35 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:17:35 up 1 day,  1:03,  1 user,  load average: 0.19, 0.13, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -63,10 +63,10 @@ int sys_gettimeofday(struct timeval *tv, struct timezone* tz) {
 int sys_execve(const char *pathname, char *const argv[], char *const envp[]) {
   if (pathname == NULL)
     return -1;
-  //naive_uload(NULL, pathname);
-  context_uload(current, pathname, argv, envp);
-  switch_boot_pcb();
-  yield();
+  naive_uload(NULL, pathname);
+  // context_uload(current, pathname, argv, envp);
+  // switch_boot_pcb();
+  // yield();
   return 0;
 }
 
