0723545cfb09c2d13f9dbf2d6f7234dc9d5eb4ba
tracer-ysyx <tracer@ysyx.org>
2024-02-09 00:35:23 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:35:23 up 1 day,  1:21,  1 user,  load average: 0.02, 0.05, 0.07

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -75,9 +75,9 @@ void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)1L);
   // context_kload(&pcb[1], hello_fun, (void *)2L);
 
-  char *argv[] = {"/bin/exec-test"}; 
+  char *argv[] = {"--skip"}; 
   char *envp[] = {NULL}; 
-  context_uload(&pcb[1], "/bin/exec-test", argv, envp);
+  context_uload(&pcb[0], "/bin/pal", argv, envp);
   switch_boot_pcb();
 
   Log("Initializing processes...");
@@ -88,8 +88,8 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-  int a = (current == &pcb[0] ? 0 : 1);
-  printf("now:%d, next:%d\n", a, !a);
+  //int a = (current == &pcb[0] ? 0 : 1);
+  //printf("now:%d, next:%d\n", a, !a);
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
