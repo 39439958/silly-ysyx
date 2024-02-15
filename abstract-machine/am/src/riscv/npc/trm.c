c2f33b07955fade8952e131fe512fb05bf425158
tracer-ysyx <tracer@ysyx.org>
2024-02-15 14:00:23 +0800

    > sim RTL ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-17-generic #17~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jan 16 14:32:32 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:00:23 up 11:40,  1 user,  load average: 0.60, 0.25, 0.14

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#include <stdio.h>
 
 extern char _heap_start;
 int main(const char *args);
@@ -9,9 +10,6 @@ extern char _pmem_start;
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 # define ebreak(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
-#define DEVICE_BASE 0xa0000000
-#define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
-
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
@@ -19,7 +17,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-  pmem_write(SERIAL_PORT, ch, 0);
+  printf("%c", ch);
 }
 
 void halt(int code) {
