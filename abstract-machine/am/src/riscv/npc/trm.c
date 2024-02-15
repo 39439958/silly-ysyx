e1d20bc4d81aec875522c6091bd2c7dd4454c56b
tracer-ysyx <tracer@ysyx.org>
2024-02-15 13:38:34 +0800

    > compile NEMU ysyx_23060103 xuli Linux silly-virtual-machine 6.5.0-17-generic #17~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jan 16 14:32:32 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:38:34 up 11:18,  1 user,  load average: 0.27, 0.19, 0.11

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -9,6 +9,9 @@ extern char _pmem_start;
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 # define ebreak(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
+#define DEVICE_BASE 0xa0000000
+#define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
+
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
@@ -16,6 +19,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
+  pmem_write(SERIAL_PORT, ch, 0);
 }
 
 void halt(int code) {
